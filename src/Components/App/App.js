4c2a97d63672d9a6480aba7692c0d152e128e7c6
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-01 11:26:09 -0500

    Create basic component architecture; add logo and background styling rules; update title

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from '../../images/barts-binge-logo.png';
+
+function App() {
+  return (
+    <main>
+      <div className='logo-container'>
+        <img src={logo} alt='Barts Binge logo' />
+      </div>
+      
+
+    </main>
+  );
+}
+
+export default App;
