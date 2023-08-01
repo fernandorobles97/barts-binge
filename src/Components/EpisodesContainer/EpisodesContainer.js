d22ab90b2de4056b79a57c06609d68f6e97e51db
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-01 15:14:36 -0500

    Add state for topRated episodes; add function that filters episodes; add button elements and header element to EpisodesContainer return

--- src/Components/EpisodesContainer/EpisodesContainer.js
+++ src/Components/EpisodesContainer/EpisodesContainer.js
@@ -1,5 +1,22 @@
-const EpisodesContainer = () => {
-  
+import { useState } from 'react'
+import './EpisodesContainer.css'
+
+const EpisodesContainer = (props) => {
+  const [topRatedEpisodes, setTopRatedEpisodes] = useState([])
+
+  const getTopRatedEpisodes = () => {
+    const topRated = props.allEpisodes.filter(episode => episode.rating >= 8)
+    setTopRatedEpisodes(topRated)
+  }
+
+  return (
+    <div className='episodes-container'>
+      <button className='view-all-button'>View All Episodes</button>
+      <h2>Top Rated Episodes</h2>
+        {/* {episodes here} */}
+      <button className='reroll-button'>Reroll Episodes</button>
+    </div>
+  )
 }
 
 export default EpisodesContainer
