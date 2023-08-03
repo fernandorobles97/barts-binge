5fb8c404208abd875e2e462bf27755afb5222d6a
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-03 10:38:23 -0500

    Add dropdown with each season 1-10 as an option; add filterBySeason function;  add seasonNumber state and useEffect with dependency for seasonNumber

--- src/Components/EpisodesContainer/AllEpisodesContainer.js
+++ src/Components/EpisodesContainer/AllEpisodesContainer.js
@@ -1,13 +1,40 @@
+import { useEffect, useState } from "react"
 import { mapCards } from "../../helpers"
 
 const AllEpisodesContainer = ({allEpisodes}) => {
+  const [seasonNumber, setSeasonNumber] = useState(1) 
 
-  const allEpisodeCards = mapCards(allEpisodes)
+  useEffect(() => {
+    
+  }, [seasonNumber])
+
+  const filterBySeason = (seasonValue) => {
+    const seasonNum = seasonValue
+    console.log('me',seasonValue)
+
+    const seasonEpisodes = allEpisodes.filter(episode => episode.season === parseInt(seasonNum))
+    console.log(seasonEpisodes)
+    return seasonEpisodes
+  }
+
+  const allEpisodeCards = mapCards(filterBySeason(seasonNumber))
 
   return (
     <div className='episodes-container'>
       <div className='buttons-header-wrapper'>
         <h2 className='container-header'>All Episodes</h2>
+        <select value={seasonNumber} className="filter-seasons" onChange={e => setSeasonNumber(e.target.value)}>
+          <option value={1}>Season 1</option>
+          <option value={2}>Season 2</option>
+          <option value={3}>Season 3</option>
+          <option value={4}>Season 4</option>
+          <option value={5}>Season 5</option>
+          <option value={6}>Season 6</option>
+          <option value={7}>Season 7</option>
+          <option value={8}>Season 8</option>
+          <option value={9}>Season 9</option>
+          <option value={10}>Season 10</option>
+        </select>
       </div>
       <div className='episodes-wrapper'>
         {allEpisodeCards}
