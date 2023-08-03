b6e0fe9451c3e03ab834431e1b24677a0ca8f6bd
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-03 15:00:15 -0500

    Add state for currentEpisode, loading, and error; invoke apiCall in useEffect that waits for getSingleEpisode data based on the location id

--- src/Components/EpisodeDetails/EpisodeDetails.js
+++ src/Components/EpisodeDetails/EpisodeDetails.js
@@ -1,5 +1,37 @@
-const EpisodeDetails = () => {
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import { getSingleEpisode } from "../../apiCalls"
 
+const EpisodeDetails = () => {
+  const [currentEpisode, setCurrentEpisode] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const location = useLocation().pathname.split('/episode/')[1]
+  const episodeLocation = parseInt(location)
+  console.log(episodeLocation)
+
+  useEffect(() => {
+    const apiCall = async () => {
+      setLoading(true)
+      try {
+        setCurrentEpisode(await getSingleEpisode(episodeLocation))
+        setLoading(false)
+      }catch(error) {
+        setError(error)
+      }
+      setLoading(false)
+    } 
+    apiCall()
+  }, [])
+  console.log(currentEpisode)
+
+  return(
+    <main>
+      <div className='buttons-header-wrapper'>
+        <h2 className='container-header'>{currentEpisode.name}</h2>
+      </div>
+    </main>
+  )
 }
 
 export default EpisodeDetails
