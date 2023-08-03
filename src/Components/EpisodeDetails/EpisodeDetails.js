2068cd8c8711f0d82bcaf7f9d90595793f7b3f9b
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-03 17:08:17 -0500

    Return all episode details

--- src/Components/EpisodeDetails/EpisodeDetails.js
+++ src/Components/EpisodeDetails/EpisodeDetails.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import { Link, useLocation } from "react-router-dom"
 import { getSingleEpisode } from "../../apiCalls"
+import './EpisodeDetails.css'
 
 const EpisodeDetails = () => {
   const [currentEpisode, setCurrentEpisode] = useState(0)
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
   const location = useLocation().pathname.split('/episode/')[1]
   const episodeLocation = parseInt(location)
@@ -12,7 +13,7 @@ const EpisodeDetails = () => {
 
   useEffect(() => {
     const apiCall = async () => {
-      setLoading(true)
+      // setLoading(true)
       try {
         setCurrentEpisode(await getSingleEpisode(episodeLocation))
         setLoading(false)
@@ -25,13 +26,29 @@ const EpisodeDetails = () => {
   }, [])
   console.log(currentEpisode)
 
-  return(
-    <main>
-      <div className='buttons-header-wrapper'>
-        <h2 className='container-header'>{currentEpisode.name}</h2>
-        <Link className='view-all-button' to='/allepisodes' >Back To All Episodes</Link>
-      </div>
-    </main>
+  return(  
+    <div className="episode-content">
+      {!loading &&
+        <section>
+          <div className='buttons-header-wrapper'>
+            <h2 className='container-header'>{currentEpisode.name}</h2>
+            <Link className='view-all-button' to='/allepisodes' >Back To All Episodes </Link>
+          </div>
+          <div className="episode-details-container">
+            <div className="details-wrapper">
+              <p>Season: {currentEpisode.season}</p>
+              <p>Episode: {currentEpisode.episode}</p>
+              <p>Description: {currentEpisode.description}</p>
+              <p>Rating: {currentEpisode.rating}</p>
+              <p>Original Airdate: {currentEpisode.airDate.slice(0, 10)}</p>
+            </div>
+            <div className="img-container">
+              <img src={currentEpisode.thumbnailUrl} alt={`${currentEpisode.name} thumbnail`} />
+            </div>
+          </div>
+        </section>
+      } 
+    </div>
   )
 }
 
