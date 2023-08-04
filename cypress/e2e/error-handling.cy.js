c4dd2016abad1434bf303e4302b11c617befd450
Fernando Robles <fernandorobles97@yahoo.com>
2023-08-04 12:41:38 -0500

    Test for 500 error

---
+++ cypress/e2e/error-handling.cy.js
@@ -0,0 +1,17 @@
+describe('error handling spec', () => {
+
+  const interceptData = (status) => {
+    cy.intercept('GET', 'https://api.sampleapis.com/simpsons/episodes', {
+      statusCode: status,
+      fixture: 'episodes.json'
+    }).as('interceptError')
+    cy.visit('http://localhost:3000')
+  };
+
+  it('shoud display a message for a 500 error', () => {
+    interceptData(500)
+    cy.wait('@interceptError').then((intercept) => {
+      cy.get('h2').contains('Error: 500 -- Please try again!')
+    })
+  })
+})
